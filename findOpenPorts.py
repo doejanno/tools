0a8e1edf5f74f77919622fd0c09df0bbac81b606
doejanno <102871931+doejanno@users.noreply.github.com>
2023-07-10 12:07:14 +0200

    Adding the Optionions to Logging
    It is now possible to create an additional log with just open ports.
    The filenames are now chosen in the __main__ function.

--- findOpenPorts.py
+++ findOpenPorts.py
@@ -2,31 +2,34 @@ import socket
 import time
 
 # function for logging
-def logText(message):
-    lg = open('loggerPorts.txt', 'a')
-    current_time = time.strftime('%H:%M:%S', time.localtime())
-    lg.write(f'{current_time} - {message} \n')
-    lg.close()
+def logText(message, filename):
+    lg = open(filename, 'a') # open a txt file to write in
+    current_time = time.strftime('%H:%M:%S', time.localtime()) #set current time
+    lg.write(f'{current_time} - {message} \n') # write message into textfile
+    lg.close() # close file
 
-def checkPortsOfInterest(HOST):
-    PORTS = [21, 25, 80, 502]
+def checkPOI(HOST, logFileAll, logFileOpen):
+    PORTS = [21, 25, 80, 502] # TCP ports
 
-    logText(f'Connetion to: {HOST}')
+    logText(f'Connetion to: {HOST}', logFileAll)
+    logText(f'Connetion to: {HOST}', logFileOpen)
     print(f'Connection to: {HOST}')
     for arg in PORTS:
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.connect((HOST, arg))
-                logText(f'Port: {arg} OPEN')
+                logText(f'Port: {arg} OPEN', logFileAll)
+                logText(f'Port: {arg}', logFileOpen)
                 print(f"Port: {arg} OPEN")
                 s.close()
         except:
-            logText(f'Port: {arg} CLOSED')
+            logText(f'Port: {arg} CLOSED', logFileAll)
             print(f'Port: {arg} CLOSED')
 
 
-def bruteForcePortSearch(HOST): # Function to brute Force oben PORTS
-    logText(f'Connection to: {HOST}')
+def bruteForcePortSearch(HOST, logFileAll, logFileOpen): # Function to brute Force oben PORTS
+    logText(f'Connection to: {HOST}', logFileAll)
+    logText(f'Connection to: {HOST}', logFileOpen)
     print(f"Connection to: {HOST}")
     PORT = 0 # Define start Index of searched Ports (privileged ports normaly are <= 1023)
     while True:
@@ -34,17 +37,20 @@ def bruteForcePortSearch(HOST): # Function to brute Force oben PORTS
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.connect((HOST, PORT))
-                logText(f'Port: {PORT} OPEN')
+                logText(f'Port: {PORT} OPEN', logFileAll)
+                logText(f'Port: {PORT} OPEN', logFileOpen)
                 print(f"Port: {PORT} OPEN")
                 s.close()
         except:
-            logText(f'Port: {PORT} CLOSED')
+            logText(f'Port: {PORT} CLOSED', logFileAll)
             print(f'Port: {PORT} CLOSED')
         PORT=PORT+1
 
 
 if __name__ == '__main__':
+    logAll = 'loggerPorts.txt' # name of log with all Ports
+    logOpen = 'loggerOpenPorts.txt' # name of log with Open Ports
     HOST = '127.0.0.1'
     # HOST = '169.254.173.4' # Insert IP form HOST of interest
-    checkPortsOfInterest(HOST)
-    bruteForcePortSearch(HOST)
+    checkPOI(HOST, logAll, logOpen)
+    bruteForcePortSearch(HOST, logAll, logOpen)
